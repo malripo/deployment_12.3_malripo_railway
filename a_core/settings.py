f53a33f9b0ccace40dde795a3edaf2f30d795f94
malripo <malripo741205@gmail.com>
2025-02-04 06:51:48 +0900

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -41,13 +41,13 @@ ENCRYPT_KEY = env('ENCRYPT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # for environment
-if ENVIRONMENT == 'development':
+if ENVIRONMENT == 'production':
     DEBUG = True
 else:
     DEBUG = False
 
 # ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', env("RENDER_EXTERNAL_HOSTNAME"),
 #                  'www.ttugttag.sbs','ttugttag.sbs',]
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1',
@@ -55,8 +55,8 @@ else:
 # CSRF_TRUSTED_ORIGINS =[ 'https://*.onrender.com',
 #                         'https://*.ttugttag.sbs/', 'https://ttugttag.sbs/',]
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','huttag.up.railway.app']
-CSRF_TRUSTED_ORIGINS =['https://huttag.up.railway.app/']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1','huttag.up.railway.app']
+# CSRF_TRUSTED_ORIGINS =['https://huttag.up.railway.app/']
 
 # CSRF_TRUSTED_ORIGINS = [ 'huttag.up.railway.app' ]
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
